98b600e9b42d70c72a498248f9b73cf1087df7f3
Maria Teresa <mariateresa.pepaj@studenti.unipd.it>
2023-03-23 19:03:33 +0100

    time limit aggiunto a two opt move

--- src/heuristics.h
+++ src/heuristics.h
@@ -43,5 +43,5 @@ int nearest_neighbor_allstart(Instance*);
 void nearest_neighbor(Instance*, int);
 Point* convex_hull(Point*, int, int*);
 void nearest_neighbor_grasp_random(Instance*, int, double);
-void two_opt_move(Instance*);
+void two_opt_move(Instance*,int );
 #endif
